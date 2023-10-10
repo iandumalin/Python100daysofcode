e40b72072697883bb6513501ad79b992e88b8dba
iandumalin <ian.dumalin@hotmail.com>
2023-10-10 10:25:21 +0200

    added day 6

---
+++ Day006/ReeborgHurdles.py
@@ -0,0 +1,27 @@
+# exercise at https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+def jump() :
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+   
+def turn_right() :
+    turn_left()
+    turn_left()
+    turn_left()
+
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
+move()
+jump()
